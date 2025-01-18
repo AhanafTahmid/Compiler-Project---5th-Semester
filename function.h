175c6a4c98753d19b95e7783efdae162a360031a
AhanafTahmid <ahanaftahmid55@gmail.com>
2025-01-19 04:04:04 +0600

    .

--- function.h
+++ function.h
@@ -4,19 +4,17 @@ using namespace std;
 
 //for the button and field
 #include "ui.h"
-#include "tree.h"
+//#include "tree.h"
 
 //Below are the functions that will run in the application
 //function age pore likhsi bole age theke ekhane define kore disi
 
-//void drawwwwwwww();
+void drawwwwwwww();
 
 void get_ans();
 void ar_input();//arithmetic expression
 void regex_input();
 
-void Easy();
-void Easy2();
 //void loading();
 void about_us();//it is the about us page
 void exit();
@@ -24,6 +22,7 @@ void menu();
 void info_start();
 void Header();
 void Footer();
+bool isOperator();
 
 
 void frame()
@@ -44,26 +43,6 @@ void frame()
 char name[200] = "";
 char cpyname[200]="";
 char exp1[200] = "";
-
-
-vector<string> readDataFromFile(char* fileName)
-{
-    vector<string>words;
-    FILE *file = fopen(fileName, "r");
-    char c;
-    string word;
-    while((c = fgetc(file)) != EOF)
-    {
-        if(c == ' ' || c == '\n')
-        {
-            words.push_back(word);
-            word = "";
-        }
-        else word.push_back(c);
-    }
-    fclose(file);
-    return words;
-}
 
 
 void type_till_go(){}
@@ -213,32 +192,6 @@ void info_start() {
 
 
 
-void Easy()
-{
-    cleardevice();
-    frame();
-
-    Footer();
-    new Field(0, 0, 800, 50, MAGENTA,WHITE, "Easy Labels");
-    Button char_one_mnt(200, 130, 600, 230, CYAN, "Type Characters");
-    //Button char_two_mnt(200, 230, 600, 330, MAGENTA, "Label 2");
-    //Button char_five_mnt(200, 330, 600, 430, CYAN, "Label 3");
-    Button back(15, 425, 130, 465, BLUE, "BACK");
-    while(true)
-    {
-        char_one_mnt.hover(RED);
-        //char_two_mnt.hover(BLUE);
-        //char_five_mnt.hover(RED);
-        back.hover(GREEN);
-
-        if(GetAsyncKeyState(VK_LBUTTON) & (0x8000 != 0))
-        {
-            if(char_one_mnt.cursor())  type_till_go();
-            else if(back.cursor())info_start();
-        }
-        if(kbhit()) getch();
-    }
-}
 void about_us()
 {
     setbkcolor(DARKGRAY);
@@ -380,57 +333,6 @@ void login()
 //---------------------------------------------------------------------------------------------------------------------------
 //---------------------------------------------------------------------------------------------------------------------------
 
-//#############################################################################################
-//Arithmetic input
-bool isOperator(char ch) {
-    return (ch == '+' || ch == '-' || ch == '*' || ch == '/');
-}
-
-
-void ar_input() {
-    setbkcolor(DARKGRAY);
-   // setbkcolor(LIGHTCYAN);
-    cleardevice();
-    //Footer();
-    settextstyle(8, 0, 2);
-    new Field(100, 350, 350, 400, GREEN,WHITE, "ENTER Expression");
-    //Button back(15, 525, 130, 565, BLUE, "Exit");
-    Input exp;
-    exp.Name(350, 350,700, 400);
-    Button submit(370,430,450,480, BLUE, "OK");
-    Button back(15, 425, 130, 480, BLUE, "BACK");
-
-    while(true) {
-        submit.hover(GREEN);
-        if(GetAsyncKeyState(VK_LBUTTON) & (0x8000 != 0)) {
-            if(exp.cursor()){ exp.getName(exp1); }
-            else if(submit.cursor()) {
-                // evaluate exprresion
-                bool ok = 0;
-                int nn = strlen(exp1);
-                if( nn == 1) ok = 1;
-                for(int i=1;i<nn;i++){
-                    if(i&1 && !isOperator(exp1[i])){
-                        ok = 1;
-                        break;
-                    }
-                    if(!(i&1) && isOperator(exp1[i])){
-                        ok = 1;
-                        break;
-                    }
-                }
-                // ok = 1 = invalid ,
-                if(ok) outtextxy(100, 200, "Invalid Expression");
-                else outtextxy(100, 200, "   Valid Expression  ");
-
-            }
-            else if(back.cursor()) {
-                menu();
-            }
-        }
-    }
-}
-
 
 
 
@@ -483,6 +385,249 @@ void regex_input() {
 
 
 
+
+
+//////treeeeeeeee
+
+
+
+char name1[200] = "";//expression
+
+struct TreeNode {
+    string value;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(string val) : value(val), left(nullptr), right(nullptr) {}
+};
+
+/////////foooter for tree 
+void Footers() {
+    setfillstyle(SOLID_FILL, CYAN);
+    settextstyle(GOTHIC_FONT, HORIZ_DIR, 1);
+    setbkcolor(CYAN);
+    outtextxy(500 - textwidth("Developed By Spartan")/2, 585 - textheight("A") / 2, "Developed By Spartan");
+    settextstyle(GOTHIC_FONT, HORIZ_DIR, 3);
+}
+
+
+void drawNode(int x, int y, const string& value, int color = WHITE) {
+    setcolor(color);
+    setfillstyle(SOLID_FILL, color);
+    circle(x, y, 20);
+    floodfill(x, y, color);
+
+    setbkcolor(color);
+    settextstyle(DEFAULT_FONT, HORIZ_DIR, 2);
+    setcolor(BLACK);
+
+    outtextxy(x - 10, y - 10, (char*)value.c_str());
+}
+
+
+void drawLine(int x1, int y1, int x2, int y2) {
+    setcolor(WHITE);
+    line(x1, y1, x2, y2);
+}
+
+void visualizeTree(TreeNode* root, int x, int y, int xOffset, int level = 0) {
+    if (!root) return;
+
+    drawNode(x, y, root->value, LIGHTBLUE);
+
+    // Draw leff
+    if (root->left) {
+        drawLine(x, y + 20, x - xOffset, y + 80);
+        visualizeTree(root->left, x - xOffset, y + 80, xOffset / 2, level + 1);
+    }
+
+    // Draw right
+    if (root->right) {
+        drawLine(x, y + 20, x + xOffset, y + 80);
+        visualizeTree(root->right, x + xOffset, y + 80, xOffset / 2, level + 1);
+    }
+}
+
+// construct a parsing tree from a mathematical expression
+TreeNode* constructTree(const string& expression) {
+    stack<TreeNode*> nodes;   // Stack for operand nodes
+    stack<char> operators;    // Stack for operators
+
+    for (char ch : expression) {
+        if (isspace(ch)) continue; // Skip spaces
+
+        // If character is a digit or variable, create a node
+        if (isalnum(ch)) {
+            nodes.push(new TreeNode(string(1, ch)));
+        }
+        // If character is an operator
+        else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+            while (!operators.empty() &&
+                   ((ch == '+' || ch == '-') || (ch == '*' || ch == '/')) &&
+                   (operators.top() == '*' || operators.top() == '/')) {
+                // Process higher precedence operators
+                char op = operators.top();
+                operators.pop();
+
+                TreeNode* right = nodes.top(); nodes.pop();
+                TreeNode* left = nodes.top(); nodes.pop();
+
+                TreeNode* newNode = new TreeNode(string(1, op));
+                newNode->left = left;
+                newNode->right = right;
+
+                nodes.push(newNode);
+            }
+            operators.push(ch);
+        }
+        // If opening parenthesis, push to operator stack
+        else if (ch == '(') {
+            operators.push(ch);
+        }
+        // If closing parenthesis, process until opening parenthesis
+        else if (ch == ')') {
+            while (!operators.empty() && operators.top() != '(') {
+                char op = operators.top();
+                operators.pop();
+
+                TreeNode* right = nodes.top(); nodes.pop();
+                TreeNode* left = nodes.top(); nodes.pop();
+
+                TreeNode* newNode = new TreeNode(string(1, op));
+                newNode->left = left;
+                newNode->right = right;
+
+                nodes.push(newNode);
+            }
+            if (!operators.empty() && operators.top() == '(') {
+                operators.pop(); // Remove the opening parenthesis
+            }
+        }
+    }
+
+    // Process any remaining operators
+    while (!operators.empty()) {
+        char op = operators.top();
+        operators.pop();
+
+        TreeNode* right = nodes.top(); nodes.pop();
+        TreeNode* left = nodes.top(); nodes.pop();
+
+        TreeNode* newNode = new TreeNode(string(1, op));
+        newNode->left = left;
+        newNode->right = right;
+
+        nodes.push(newNode);
+    }
+
+    // The remaining node is the root of the tree
+    return nodes.empty() ? nullptr : nodes.top();
+}
+
+
+void visualizeParsingTree(const string& expression) {
+    cleardevice();
+    TreeNode* root = constructTree(expression);
+
+    if (!root) {
+        cout << "Invalid expression!\n";
+        return;
+    }
+
+    // Visualize the tree
+    visualizeTree(root, getmaxx() / 2, 50, getmaxx() / 4);
+}
+
+
+void tree_input() {
+    setbkcolor(LIGHTCYAN);
+    cleardevice();
+    Footers();
+    settextstyle(8, 0, 2);
+    new Field(100, 350, 350, 400, GREEN,WHITE, "ENTER Expression");
+    Input exp;
+    exp.Name(350, 350,700, 400);
+    Button submit(370,430,450,480, BLUE, "OK");
+    Button back(15, 425, 130, 480, BLUE, "BACK");
+
+    while(true) {
+        int tr=0;
+        submit.hover(GREEN);
+        if(GetAsyncKeyState(VK_LBUTTON) & (0x8000 != 0)) {
+            if(exp.cursor()){ exp.getName(name1); tr=1; }
+            else if(submit.cursor()) {
+                visualizeParsingTree(name1);
+                 Button back(15, 425, 130, 480, BLUE, "BACK");
+            }
+            else if(back.cursor()) {
+                menu();
+            }
+        }
+    }
+}
+
+
+void drawwwwwwww() {
+    //int gd = DETECT, gm;
+    //initgraph(&gd, &gm, "");
+
+    tree_input();
+    //string expression = "(a+b)+c+d";
+    //visualizeParsingTree(expression);
+}
+
+
+
+
+//#############################################################################################
+//Arithmetic input
+bool isOperator(char ch) {
+    return (ch == '+' || ch == '-' || ch == '*' || ch == '/');
+}
+
+
+void ar_input() {
+    setbkcolor(DARKGRAY);
+   // setbkcolor(LIGHTCYAN);
+    cleardevice();
+    //Footer();
+    settextstyle(8, 0, 2);
+    new Field(100, 350, 350, 400, GREEN,WHITE, "ENTER Expression");
+    //Button back(15, 525, 130, 565, BLUE, "Exit");
+    Input exp;
+    exp.Name(350, 350,700, 400);
+    Button submit(370,430,450,480, BLUE, "OK");
+    Button back(15, 425, 130, 480, BLUE, "BACK");
+
+    while(true) {
+        submit.hover(GREEN);
+        if(GetAsyncKeyState(VK_LBUTTON) & (0x8000 != 0)) {
+            if(exp.cursor()){ exp.getName(exp1); }
+            else if(submit.cursor()) {
+                // evaluate exprresion
+                bool ok = 0;
+                int nn = strlen(exp1);
+                if( nn == 1) ok = 1;
+                for(int i=1;i<nn;i++){
+                    if(i&1 && !isOperator(exp1[i])){
+                        ok = 1;
+                        break;
+                    }
+                    if(!(i&1) && isOperator(exp1[i])){
+                        ok = 1;
+                        break;
+                    }
+                }
+                // ok = 1 = invalid ,
+                if(ok) outtextxy(100, 200, "Invalid Expression");
+                else outtextxy(100, 200, "   Valid Expression  ");
+            }
+            else if(back.cursor()) {
+                menu();
+            }
+        }
+    }
+}
 
 
 
